800971a6226978bbe06373938eae01f56cadcbc9
Marwankh12 <92727445+Marwankh12@users.noreply.github.com>
2022-09-06 11:29:35 +0200

    Update [id].js

--- pages/[id].js
+++ pages/[id].js
@@ -5,9 +5,9 @@ export default function id(data){
     return(
 <>
 <Head>
-<script src="https://kit.fontawesome.com/a076d05399.js" crossOrigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/a076d05399.js" crossOrigin="anonymous" async ></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous" defer ></script>
 </Head>
 <div className ='flexr'>
 
